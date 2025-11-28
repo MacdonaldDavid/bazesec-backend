3e6933c740dd2640a8a9d47413dc46ca4d5844a1
VrtDave <vrtblaize@gmail.com>
2025-11-28 21:34:49 +0100

    Added Vercel to CORS policy

--- BazeSec/Program.cs
+++ BazeSec/Program.cs
@@ -30,10 +30,14 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+        policy.WithOrigins(
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://bazesec-frontend.vercel.app"
+    )
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials();
     });
 });
 
